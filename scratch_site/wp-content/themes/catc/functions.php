837f997ebe211864c25ca1de670a40289cb49fe8
Tassanai <amm_25204@hotmail.com>
2020-05-20 01:11:38 +0700

    add custom logo function

--- scratch_site/wp-content/themes/catc/functions.php
+++ scratch_site/wp-content/themes/catc/functions.php
@@ -33,3 +33,18 @@ register_nav_menus(
         'footer-menu' => 'Footer Menu Location',
     )
 );
+
+// Theme Custom Logo
+function themename_custom_logo_setup()
+{
+    $defaults = array(
+        'height'      => 85,
+        'width'       => 400,
+        'flex-height' => true,
+        'flex-width'  => true,
+        'header-text' => array( 'site-title', 'site-description' ),
+    );
+    add_theme_support( 'custom-logo', $defaults );
+}
+
+add_action( 'after_setup_theme', 'themename_custom_logo_setup' );
--- scratch_site/wp-content/themes/catc/header-secondary.php
+++ scratch_site/wp-content/themes/catc/header-secondary.php
@@ -9,6 +9,20 @@
 <body>
     <header>
         <div class="container">
+            <div>
+                <?php
+                    if ( function_exists( 'the_custom_logo' ) ) {
+                        // the_custom_logo();
+                        $custom_logo_id = get_theme_mod( 'custom_logo' );
+                        $logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+                        if ( has_custom_logo() ) {
+                            echo '<img src="' . esc_url( $logo[0] ) . '" alt="' . get_bloginfo( 'name' ) . '">';
+                        } else {
+                            echo '<h1>'. get_bloginfo( 'name' ) .'</h1>';
+                        }
+                    }
+                ?>
+            </div>
             <?php
                 wp_nav_menu(
                     array(
