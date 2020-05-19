132448fbbfa0c0af7f0e32bfcc08a11365f8239f
Tassanai <amm_25204@hotmail.com>
2020-05-20 00:52:56 +0700

    add css page wrap

--- scratch_site/wp-content/themes/catc/front-page.php
+++ scratch_site/wp-content/themes/catc/front-page.php
@@ -1,8 +1,10 @@
 <?php get_header('secondary'); ?>
 
-<div class="container">
-    <h1><?php the_title(); ?></h1>
-    <?php get_template_part('includes/section', 'content'); ?>
-</div>
+<section class="page-wrap">
+    <div class="container">
+        <h1><?php the_title(); ?></h1>
+        <?php get_template_part('includes/section', 'content'); ?>
+    </div>
+</section>
 
 <?php get_footer(); ?>
